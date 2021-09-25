d64cd706b62e9678c044bdbc7aa209cf999f71d5
klugh <klugh@student.ethz.ch>
2021-09-25 10:24:24 +0200

    add dict2json

--- modun/file_io.py
+++ modun/file_io.py
@@ -6,3 +6,8 @@ def json2dict(json_path: Path) -> dict:
     with open(json_path, 'rt') as json_file:
         json_config = json.load(json_file)
     return json_config
+
+
+def dict2json(out_path: Path, d: dict):
+    with open(out_path, 'w') as outfile:
+        json.dump(d, outfile, indent=2)
